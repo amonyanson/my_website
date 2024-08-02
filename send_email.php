f84cb66a2fae89fb402c67c3bfba33cd72810b48
Anson Osembo <amonianson@gmail.com>
2024-08-02 12:41:52 +0300

    First Commit

---
+++ send_email.php
@@ -0,0 +1,24 @@
+<?php
+  $to = "amonianson@gmail.com"; // your Gmail account
+  $subject = "Contact Form Submission";
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+  $body = "<html><body>";
+  $body .= "<h2>Contact Form Submission</h2>";
+  $body .= "<p>Name: $name</p>";
+  $body .= "<p>Email: $email</p>";
+  $body .= "<p>Message: $message</p>";
+  $body .= "</body></html>";
+
+  mail($to, $subject, $body, $headers);
+
+  header("Location: contact.html?sent=true");
+  exit;
+?>
